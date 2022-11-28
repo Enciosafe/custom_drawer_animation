4240b9f9b39238ca9b9a8b2273f9dc2cb4f7a641
Stas <finneganlucky@gmail.com>
2022-11-28 16:49:50 +0100

    main

---
+++ utils.js
@@ -0,0 +1,26 @@
+export const routes = [
+    'STAN',
+    'JAKE',
+    'JULY',
+    'MIKE',
+    'JOSH',
+    'BILL',
+    'HANNA',
+];
+export const links = ['first', 'second', 'the next after second'];
+export const colors = [
+    '#00A878',
+    '#D8F1A0',
+    '#F3C178',
+    '#FE5E41',
+    '#E07BE0',
+    '#E3DFFF',
+    '#2176AE',
+    '#f9cdad',
+    '#c8c8a9',
+    '#83af9b',
+    '#ecd078',
+    '#d95b43',
+    '#c02942',
+    '#53777a',
+];
